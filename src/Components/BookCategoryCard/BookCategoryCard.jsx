1f85b17ab1afe8f407ef14c6fb9ba09215ecfb48
FrancisMurayaDev <francismurayadev@gmail.com>
2025-03-08 21:35:27 +0300

    learning how to use functional components

---
+++ src/Components/BookCategoryCard/BookCategoryCard.jsx
@@ -0,0 +1,12 @@
+function BookCategoryCard () {
+  return (
+  <div>
+    <div>
+      <img src="#" alt="An Image of a Book" />
+    </div>
+    <h2>Book Category</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus sunt eius incidunt, distinctio maxime labore.</p>
+  </div>)
+}
+
+export default BookCategoryCard
---
+++ src/Components/Greet/Greet.jsx
@@ -0,0 +1,9 @@
+export default function Greet () {
+  return (
+    <div>
+      <h2>Hello There!</h2>
+      <p>Welcome of React Mastery. I find react library to be so amazing.</p>
+      <a href="#">Learn More</a>
+    </div>
+  )
+}
