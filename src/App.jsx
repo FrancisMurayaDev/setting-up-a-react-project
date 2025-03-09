ef6806f3347785474bf6c03f42de050c1e7736e4
FrancisMurayaDev <francismurayadev@gmail.com>
2025-03-09 10:59:50 +0300

    install react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,9 @@
-import Button from "./Components/Button/Button";
-
-function Greet ({userName}) {
-if (userName === undefined) {
-  return <h1>Hello Guest</h1>;
-} else {
-  return <h1> Hello {userName}</h1>;
-}
+function App() {
+  return (
+    <div>
+      <p>Good</p>
+    </div>
+  )
 }
 
-function DisplayVideo ({age}) {
-  if (age <= 17) {
-    return <p>Access Denied, Go and watch Tom and Jerry!</p>
-  } else {
-    return <iframe width="560" height="315" src="https://www.youtube.com/embed/G1p6rlDCxq0?si=Qlv0iyHOCOniXUFb" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-  }
-}
-
-function App () {
- return (
-  <>
-  {/* <Greet />
-  <Greet userName='Kamau' />
-  <Greet userName={`Jose`} />
-  <DisplayVideo age={12}/>
-  <DisplayVideo age={22}/> */}
-
-  <Button />
-  </>
- )
-}
-
-export default App;
+export default App
